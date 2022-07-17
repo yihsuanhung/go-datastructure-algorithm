763cfebf1545dec03a676d69df9659295447ef4f
Simon <simon820225@gmail.com>
2022-07-17 17:08:19 +0800

    linked list

--- linkedlist/linkedlinst.go
+++ linkedlist/linkedlinst.go
@@ -1,5 +1,7 @@
 package linkedlist
 
+import "fmt"
+
 type Node struct {
 	Value interface{}
 	Next  *Node
@@ -40,18 +42,105 @@ func (ll *LinkedList) Prepend(node *Node) {
 
 }
 
-func Append() {}
+func (ll *LinkedList) Append(node *Node) {
+	isEmpty := ll.isEmpty()
 
-func Pop() {}
+	if isEmpty {
+		ll.head = node
+		ll.tail = node
+	} else {
+		ll.tail.Next = node
+		ll.tail = node
+	}
+}
 
-func PopFirst() {}
+func (ll *LinkedList) Pop() (*Node, error) {
+	isEmpty := ll.isEmpty()
 
-func (ll LinkedList) Head() *Node {
+	if isEmpty {
+		return nil, fmt.Errorf("list is empty")
+	}
+
+	if ll.head == ll.tail {
+		n := ll.head
+		ll.head = nil
+		ll.tail = nil
+		return n, nil
+	}
+
+	var previous *Node
+
+	for current := ll.head; current != ll.tail; current = current.Next {
+
+		previous = current
+
+	}
+
+	n := previous.Next
+	previous.Next = nil
+	ll.tail = previous
+
+	return n, nil
+
+}
+
+func (ll *LinkedList) PopFirst() (*Node, error) {
+
+	isEmpty := ll.isEmpty()
+
+	if isEmpty {
+		return nil, fmt.Errorf("list is empty")
+	}
+
+	if ll.head == ll.tail {
+		n := ll.head
+		ll.head = nil
+		ll.tail = nil
+		return n, nil
+	}
+
+	n := ll.head
+
+	ll.head = ll.head.Next
+	return n, nil
+
+}
+
+func (ll *LinkedList) Head() *Node {
 	return ll.head
 }
 
-func (ll LinkedList) Tail() *Node {
+func (ll *LinkedList) Tail() *Node {
 	return ll.tail
 }
 
-func Remove() {}
+func (ll *LinkedList) Remove(node *Node) error {
+
+	if node == ll.head {
+		_, err := ll.PopFirst()
+		return err
+	}
+
+	if node == ll.tail {
+		ll.Pop()
+		return nil
+	}
+
+	var previous *Node
+
+	current := ll.head
+
+	for ; current != node && current != nil; current = current.Next {
+
+		previous = current
+
+	}
+
+	if current == nil {
+		return fmt.Errorf("%v is not in the linked list", node)
+	}
+
+	previous.Next = previous.Next.Next
+	return nil
+
+}
--- main.go
+++ main.go
@@ -15,8 +15,14 @@ func print(l *linkedlist.LinkedList) {
 
 func main() {
 	ll := linkedlist.New()
-	node := linkedlist.NewNode("node1")
-	ll.Prepend(node)
+	node1 := linkedlist.NewNode("node1")
+	node2 := linkedlist.NewNode("node2")
+	node4 := linkedlist.NewNode("node4")
+	ll.Prepend(node1)
+	ll.Prepend(node2)
+	ll.Append(node4)
+
+	ll.Remove(node1)
 
 	print(ll)
 
